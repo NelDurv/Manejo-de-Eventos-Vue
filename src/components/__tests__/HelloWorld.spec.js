2cd2eedf46d266ea8fde241e81177cea7fd16f97
NelDurv <noslen.dur@gmail.com>
2026-06-13 01:41:49 -0400

    creacion de componentes

--- src/components/__tests__/HelloWorld.spec.js
+++ src/components/__tests__/HelloWorld.spec.js
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import HelloWorld from '../Tarjeta.vue/index.js'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
